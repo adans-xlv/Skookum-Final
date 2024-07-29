80c8ffd8caebd0aec77f6cc1ca6aa013db1adff9
Emmanuel-Oite <143942455+Emmanuel-Oite@users.noreply.github.com>
2024-07-29 13:19:32 +0300

    Fixed the nav bar

--- contact.php
+++ contact.php
@@ -1,95 +1,69 @@
 <?php
+if (!$_POST) exit;
 
-// CHANGE EMAIL ADDRESS ON LINE 54.
-
-
-if(!$_POST) exit;
-
-// Email address verification, do not edit.
+// Email address verification
 function isEmail($email) {
-	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
-$name     = $_POST['name'];
-$email    = $_POST['email'];
-$phone   = $_POST['phone'];
-$subject  = $_POST['subject'];
-$comments = $_POST['comments'];
-$verify   = $_POST['verify'];
+// Sanitize input data
+$name     = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$email    = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$phone    = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+$subject  = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
+$comments = filter_input(INPUT_POST, 'comments', FILTER_SANITIZE_STRING);
 
-if(trim($name) == '') {
-	echo '<div class="error_message">You must enter your name.</div>';
-	exit();
-} else if(trim($email) == '') {
-	echo '<div class="error_message">Please enter a valid email address.</div>';
-	exit();
-} else if(trim($phone) == '') {
-	echo '<div class="error_message">Please enter a valid phone number.</div>';
-	exit();
-} else if(!is_numeric($phone)) {
-	echo '<div class="error_message">Phone number can only contain digits.</div>';
-	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="error_message">You have enter an invalid e-mail address, try again.</div>';
-	exit();
+// Form validation
+if (empty($name)) {
+    echo '<div class="error_message">You must enter your name.</div>';
+    exit();
+} elseif (empty($email)) {
+    echo '<div class="error_message">Please enter a valid email address.</div>';
+    exit();
+} elseif (empty($phone)) {
+    echo '<div class="error_message">Please enter a valid phone number.</div>';
+    exit();
+} elseif (!preg_match('/^[0-9]+$/', $phone)) {
+    echo '<div class="error_message">Phone number can only contain digits.</div>';
+    exit();
+} elseif (!isEmail($email)) {
+    echo '<div class="error_message">You have entered an invalid e-mail address, try again.</div>';
+    exit();
+} elseif (empty($comments)) {
+    echo '<div class="error_message">Please enter your message.</div>';
+    exit();
 }
 
-if(trim($comments) == '') {
-	echo '<div class="error_message">Please enter your message.</div>';
-	exit();
-}
+// Configuration option
+// Enter the email address that you want emails to be sent to
+$address = "opoti63@gmail.com";
 
-if(get_magic_quotes_gpc()) {
-	$comments = stripslashes($comments);
-}
-
-
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
-// Example $address = "joe.doe@yourdomain.com";
-
-//$address = "example@example.com";
-$address = "your-email-address@your-domain.com";
-
-
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
-
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
-
+// Email subject
 $e_subject = 'You\'ve been contacted by ' . $name . '.';
 
-
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
-
+// Compose the email content
 $e_body = "You have been contacted by $name with regards to $subject, their additional message is as follows." . PHP_EOL . PHP_EOL;
 $e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
 $e_reply = "You can contact $name via email, $email or via phone $phone";
 
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+$msg = wordwrap($e_body . $e_content . $e_reply, 70);
 
+// Email headers
 $headers = "From: $email" . PHP_EOL;
 $headers .= "Reply-To: $email" . PHP_EOL;
 $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-if(mail($address, $e_subject, $msg, $headers)) {
-
-	// Email has sent successfully, echo a success page.
-
-	echo "<fieldset>";
-	echo "<div id='success_page'>";
-	echo "<h4 class='highlight'>Thank you <strong>$name</strong>, your message has been submitted to us.</h4>";
-	echo "</div>";
-	echo "</fieldset>";
-
+// Send email
+if (mail($address, $e_subject, $msg, $headers)) {
+    echo "<fieldset>";
+    echo "<div id='success_page'>";
+    echo "<h4 class='highlight'>Thank you <strong>$name</strong>, your message has been submitted to us.</h4>";
+    echo "</div>";
+    echo "</fieldset>";
 } else {
-
-	echo 'ERROR!';
-
+    echo 'ERROR!';
 }
